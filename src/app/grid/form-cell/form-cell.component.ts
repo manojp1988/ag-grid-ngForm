9edb626c9516e30f1892526a0dc6697eb72a23a2
Manoj P <manojp1988@gmail.com>
2019-02-05 20:04:51 +0530

    Change from array to object for each row

--- src/app/grid/form-cell/form-cell.component.ts
+++ src/app/grid/form-cell/form-cell.component.ts
@@ -1,36 +1,30 @@
-import {Component} from '@angular/core';
-import {FormArray} from "@angular/forms";
+import { Component } from '@angular/core';
+import { FormArray } from '@angular/forms';
 
 @Component({
-    selector: 'app-form-cell',
-    template: `
-        <div *ngIf="formGroup" [formGroup]="formGroup">
-            <mat-form-field [floatLabel]="'never'" style="width: 100%">
-                <input matInput [formControlName]="key" [id]="key" placeholder="Enter {{columnName}}">
-            </mat-form-field>
-        </div>
-    `
+  selector: 'app-form-cell',
+  template: `
+    <div *ngIf="formArray" [formGroup]="formArray.at(index)">
+      <mat-form-field [floatLabel]="'never'" style="width: 100%">
+        <input matInput [formControlName]="key" placeholder="Enter {{ columnName }}" />
+      </mat-form-field>
+    </div>
+  `
 })
 export class FormCellComponent {
-    formGroup: FormArray;
-    key;
-    private value;
-    columnName: string;
-    private rowId: number;
+  formArray: FormArray;
+  key: string;
+  index: number;
+  columnName: string;
 
-    agInit(params: any) {
-        this.columnName = params.column.colDef.headerName;
-        this.key = params.context.createKey(params.columnApi, params.column);
-        this.value = params.value;
-        this.rowId = params.node.id;
-    }
+  agInit(params: any) {
+    this.columnName = params.column.colDef.headerName;
+    this.key = params.context.createKey(params.columnApi, params.column);
+    this.index = params.rowIndex;
+  }
 
-    refresh(params: any): boolean {
-        this.formGroup = params.context.formGroup.controls[this.rowId];
-
-        // this could also be done in GridComponent.createFormControls, but the cell component could be doing something with
-        // the value, so it feels more natural that the control value be set here
-        this.formGroup.at(this.key).patchValue(this.value);
-        return true;
-    }
+  refresh(params: any): boolean {
+    this.formArray = params.context.formArray;
+    return true;
+  }
 }
